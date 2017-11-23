cff01486e206a373a536face5a0241c6a8d000cc
roscale <roscaalex19@gmail.com>
2017-11-23 16:26:31 +0100

    ls format

--- client/ls.go
+++ client/ls.go
@@ -5,6 +5,7 @@ import (
 	"github.com/urfave/cli"
 	"os"
 	"sort"
+	"math"
 )
 
 func ls(c *cli.Context) error {
@@ -14,9 +15,33 @@ func ls(c *cli.Context) error {
 	sort.Sort(ByName(files))
 
 	for _, file := range files {
-		fmt.Printf("\tSize: %.2f kb\t %v\n", float32(file.Size())/1024, file.Name())
-		// TODO format the the size in b/kb/mb/gb and take care of tabs
+		if file.IsDir() {
+			subDir, _ := os.Open(file.Name())
+			names, _ := subDir.Readdirnames(1000)
+			nItems := len(names)
+			
+			fmt.Printf("\t%v items\t\t\t\t%v\n", nItems, file.Name())
 
+		} else {
+			var size float64
+			var unit string
+
+			if file.Size() >= int64(math.Pow(1000, 3)) {
+				size = float64(file.Size()) / math.Pow(1000, 3)
+				unit = "GB"
+			} else if file.Size() >= int64(math.Pow(1000, 2)) {
+				size = float64(file.Size()) / math.Pow(1000, 2)
+				unit = "MB"
+			} else if file.Size() >= 1000 {
+				size = float64(file.Size()) / 1000
+				unit = "KB"
+			} else {
+				size = float64(file.Size())
+				unit = "B"
+			}
+
+			fmt.Printf("\tSize: %.2f%v\t%v\n", size, unit, file.Name())
+		}
 	}
 	return nil
 }
