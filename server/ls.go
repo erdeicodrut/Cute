182782b1929003218a921bdc96b991ef0723023d
coddy <codrut@hackermail.com>
2017-12-02 15:40:45 +0200

    Pusing/Pulling works

--- server/ls.go
+++ server/ls.go
@@ -1,53 +1,41 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"sort"
-	"math"
-	"net"
 	"encoding/json"
+	"fmt"
+	"net"
+	"os"
 )
 
+var filesAll []string
+
 func ls(conn net.Conn) {
-	dir, _ := os.Open(configData.STORAGE_PATH)
-	files, _ := dir.Readdir(100)
 
-	sort.Sort(ByName(files))
+	getFiles("./")
 
-	var ls string
+	json.NewEncoder(conn).Encode(filesAll)
 
-	for _, file := range files {
-		if file.IsDir() {
-			subDir, _ := os.Open(file.Name())
-			names, _ := subDir.Readdirnames(1000)
-			nItems := len(names)
+}
 
-			ls += fmt.Sprintf("\t%v items\t\t\t\t%v\n", nItems, file.Name())
+func getFiles(filename string) {
+	fileTemp, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer fileTemp.Close()
+	file, _ := fileTemp.Stat()
 
-		} else {
-			var size float64
-			var unit string
-
-			if file.Size() >= int64(math.Pow(1000, 3)) {
-				size = float64(file.Size()) / math.Pow(1000, 3)
-				unit = "GB"
-			} else if file.Size() >= int64(math.Pow(1000, 2)) {
-				size = float64(file.Size()) / math.Pow(1000, 2)
-				unit = "MB"
-			} else if file.Size() >= 1000 {
-				size = float64(file.Size()) / 1000
-				unit = "KB"
-			} else {
-				size = float64(file.Size())
-				unit = "B"
-			}
-
-			ls += fmt.Sprintf("%.2f%v\t\t%v\n", size, unit, file.Name())
-		}
+	if !file.IsDir() {
+		filesAll = append(filesAll, fileTemp.Name())
+		return
 	}
 
-	json.NewEncoder(conn).Encode(Message{
-		Data: []byte(ls),
-	})
+	files, _ := fileTemp.Readdir(-1)
+
+	for _, tempFile := range files {
+		fmt.Println(tempFile.Name())
+		getFiles(filename + "/" + tempFile.Name())
+	}
+
 }
